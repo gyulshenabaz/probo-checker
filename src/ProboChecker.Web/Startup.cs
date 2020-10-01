a1e04f9755d2eee767137247c8262b0f87c36883
Gyulshen Abaz <weirdoo2001@gmail.com>
2020-10-01 10:49:32 +0300

    Add Web project files

---
+++ src/ProboChecker.Web/Startup.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProboChecker.DataAccess.Context;
+
+namespace ProboChecker.Web
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }        
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ProboCheckerDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+        }
+    }
+}
